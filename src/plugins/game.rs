9540adbacfa574e753fb0ffa08016a57c603aa27
Osanshoouo <osanshoouo@proton.me>
2025-12-20 18:53:27 +0100

    Basic setup to render 3D scene with dedicated ui camera

--- src/plugins/game.rs
+++ src/plugins/game.rs
@@ -5,16 +5,25 @@ use bevy::{
     render::render_resource::AsBindGroup, shader::ShaderRef,
 };
 
-use bevy::{app::App, prelude::*};
-use std::f32::consts::FRAC_PI_2;
-use std::f32::consts::TAU;
-
 use crate::prelude::cube::Object;
 use crate::prelude::player::Player;
 use crate::prelude::player_camera::CameraSensitivity;
+use bevy::{app::App, prelude::*};
+use bevy::{color::palettes::css::*, prelude::*};
+use std::f32::consts::FRAC_PI_2;
+use std::f32::consts::TAU;
 
 pub(crate) fn plugin(app: &mut App) {
-    app.add_systems(Startup, setup);
+    app.add_systems(
+        Startup,
+        (
+            setup,
+            // spawn_view_model,
+            // spawn_world_model,
+            // spawn_lights,
+            // spawn_text,
+        ),
+    );
     // Your game logic here
     // setup;
     // rotation;
@@ -58,7 +67,6 @@ fn setup(
         Transform::from_xyz(4.0, 8.0, 4.0),
     ));
 }
-
 pub(crate) fn rotation(time: Res<Time>, mut cubes: Query<(&mut Transform, &Object)>) {
     for (mut transform, cube) in &mut cubes {
         transform.translation.x = 0.0;
@@ -68,4 +76,109 @@ pub(crate) fn rotation(time: Res<Time>, mut cubes: Query<(&mut Transform, &Objec
         transform.rotate_x(0.5 * TAU * time.delta_secs());
         transform.rotate_z(0.5 * TAU * time.delta_secs());
     }
+}
+
+#[derive(Debug, Component)]
+struct WorldModelCamera;
+
+/// Used implicitly by all entities without a `RenderLayers` component.
+/// Our world model camera and all objects other than the player are on this layer.
+/// The light source belongs to both layers.
+const DEFAULT_RENDER_LAYER: usize = 0;
+
+/// Used by the view model camera and the player's arm.
+/// The light source belongs to both layers.
+const VIEW_MODEL_RENDER_LAYER: usize = 1;
+
+fn spawn_view_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+    asset_server: Res<AssetServer>,
+) {
+    let text_font = TextFont::default();
+    let arm = meshes.add(Cuboid::new(0.1, 0.1, 0.5));
+    let arm_material = materials.add(Color::from(tailwind::TEAL_200));
+
+    let texture_handle = asset_server.load("assets/holdingRat.png");
+    let texture_atlas = TextureAtlasLayout::from_grid(UVec2::splat(24), 7, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn((
+        Player,
+        Transform::from_xyz(0.0, 1.0, 0.0),
+        Visibility::default(),
+        children![
+            (
+                WorldModelCamera,
+                Camera3d::default(),
+                Projection::from(PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }),
+            ),
+            (
+                Camera2d::default(),
+                Camera {
+                    // Bump the order to render on top of the world model.
+                    order: 1,
+                    ..default()
+                },
+                // Projection::from(PerspectiveProjection {
+                //     fov: 70.0_f32.to_radians(),
+                //     ..default()
+                // }),
+                // Only render objects belonging to the view model.
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            ),
+            (
+                ImageNode::from_atlas_image(
+                    texture_handle,
+                    TextureAtlas::from(texture_atlas_handle),
+                ),
+                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            ),
+        ],
+    ));
+}
+
+fn spawn_lights(mut commands: Commands) {
+    commands.spawn((
+        PointLight {
+            color: Color::from(tailwind::ROSE_300),
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(-2.0, 4.0, -0.75),
+        // The light source illuminates both the world model and the view model.
+        RenderLayers::from_layers(&[DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER]),
+    ));
+}
+
+fn spawn_world_model(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
+    let cube = meshes.add(Cuboid::new(2.0, 0.5, 1.0));
+    let material = materials.add(Color::WHITE);
+
+    // The world model camera will render the floor and the cubes spawned in this system.
+    // Assigning no `RenderLayers` component defaults to layer 0.
+
+    commands.spawn((Mesh3d(floor), MeshMaterial3d(material.clone())));
+
+    commands.spawn((
+        Mesh3d(cube.clone()),
+        MeshMaterial3d(material.clone()),
+        Transform::from_xyz(0.0, 0.25, -3.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(cube),
+        MeshMaterial3d(material),
+        Transform::from_xyz(0.75, 1.75, 0.0),
+    ));
 }
